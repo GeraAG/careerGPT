d83c3318dc2900ecf07b6b35baf31ed8a6d6a9b1
GeraAG <ag.andrew.gerasimenko@gmail.com>
2024-04-18 14:25:44 +0300

    added sign up view

--- src/careerGPT/views.py
+++ src/careerGPT/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def home_view(request):
     return render(request, "pages/home.html", {})
+
+def signup_view(request):
+    first_name = request.POST["first_name"]
+    last_name = request.POST["last_name"]
+    email = request.POST["email"]
+    password = request.POST["password"]
+    user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
+    user.save()
