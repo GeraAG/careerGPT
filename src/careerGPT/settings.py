f411938105aca893c971da3a9354a5945ed9d557
GeraAG <ag.andrew.gerasimenko@gmail.com>
2024-04-24 00:01:40 +0300

    file input css

--- src/careerGPT/settings.py
+++ src/careerGPT/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-SATICFILES_DIRS = [
+
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
--- src/careerGPT/views.py
+++ src/careerGPT/views.py
@@ -11,12 +11,12 @@ def home_view(request):
     if request.method == "POST":
         return render(request, "pages/home.html", {})
 
-    '''
-    completion = client.chat.completions.create(
+
+    '''completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        {"role": "system", "content": "You are a HR consultant, that helps workers to get hired at the jobs they seek."},
+        {"role": "user", "content": "show me a website design where user uploads pdf of a resume and gets information on how to improve it in order to get hired"}
     ]
     )
 
