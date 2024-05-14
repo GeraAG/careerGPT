c5c67efc812b6f74b126232870056301c5dbbce5
GeraAG <ag.andrew.gerasimenko@gmail.com>
2024-05-13 22:22:41 -0400

    js changes

--- src/careerGPT/views.py
+++ src/careerGPT/views.py
@@ -18,10 +18,6 @@ def home_view(request):
 
         if form.is_valid():
             file = request.FILES['pdf_file']
-            ext = os.path.splitext(file.name)[1]
-            if ext.lower() not in ('.pdf',):
-                form.add_error('pdf_file', 'Only PDF files are allowed.')
-                return render(request, 'pages/home.html', {'form': form})
             fss = FileSystemStorage()
             filename = fss.save(file.name, file)
             uploaded_file_url = fss.url(filename)
@@ -66,13 +62,3 @@ def home_view(request):
         form = UploadForm()
     context = {'form': form}
     return render(request, 'pages/home.html', context)
-
-    '''completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a HR consultant, that helps workers to get hired at the jobs they seek. You are provided text in CV. Give advice on how to improve it."},
-        {"role": "user", "content": "show me a website design where user uploads pdf of a resume and gets information on how to improve it in order to get hired"}
-    ]
-    )
-
-    print(completion.choices[0].message)'''
--- src/static/js/upload.js
+++ src/static/js/upload.js
@@ -1,43 +1,42 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('upload-form');
-    const uploadButton = document.getElementById('upload-button');
-    const loadingSpinner = document.getElementById('loading-spinner');  // Add a loading spinner element
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent default form submission
+function submitForm(formData) {
+    fetch('/', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'X-CSRFToken': document.querySelector('input[name="csrfmiddlewaretoken"]').value
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Display the processed data
 
-        const formData = new FormData(form);
+        const paragraphs = data.message.split('\n');
+        let htmlContent = '';
+        for (const paragraph of paragraphs) {
+            htmlContent += `<p class="text-base font-light leading-relaxed mt-0 mb-4 text-copper-canyon-950">${paragraph}</p>`;
+        }
 
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '');
+        document.getElementById('upload-form-response').innerHTML = htmlContent;
+        // Show the "Try Again" button
+        document.getElementById('top-separator').style.display = 'block';
+    })
+    .catch(error => console.error('Error:', error));
+}
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('upload-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var formData = new FormData(this);
+        submitForm(formData);
 
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                const response = JSON.parse(xhr.responseText);
-                const paragraphs = response.message.split('\n');
-                let htmlContent = '<div class="relative flex py-5 items-center"><div class="flex-grow border-t border-copper-canyon-400"></div><span class="flex-shrink mx-4 text-copper-canyon-950">Ideas</span><div class="flex-grow border-t border-copper-canyon-400"></div></div>';
-                for (const paragraph of paragraphs) {
-                    htmlContent += `<p class="text-base font-light leading-relaxed mt-0 mb-4 text-copper-canyon-950">${paragraph}</p>`;
-                }
+    });
 
-                document.getElementById('upload-form-response').innerHTML = htmlContent;
-            } else {
-                let errorUpload = '<div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"><strong class="font-bold">Error uploading file</strong></div>';
-
-                document.getElementById('upload-form-response').innerHTML = errorUpload;
-                console.error('Upload failed:', xhr.statusText);
-            }
-            uploadButton.disabled = false;
-            loadingSpinner.style.display = 'none';
-        };
-
-        xhr.onerror = function(error) {
-            console.error('Network error:', error);
-            document.getElementById('upload-form-response').innerHTML = 'Network Error';
-        };
-
-        uploadButton.disabled = true;
-        loadingSpinner.style.display = 'block';
-
-        xhr.send(formData);
+    // Add event listener for the "Try Again" button
+    document.getElementById('try-again-button').addEventListener('click', function(event) {
+        event.preventDefault();
+        // Set the value of the hidden input field to indicate "Try Again"
+        document.getElementById('tryAgainInput').value = '1';
+        // Submit the form
+        var formData = new FormData(document.getElementById('upload-form'));
+        submitForm(formData);
     });
 });
